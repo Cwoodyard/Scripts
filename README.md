8bf0f65365749c38b2ac25fb5754806736197f4b
Cwoodyard <cole.woodyard@gmail.com>
2025-10-14 22:09:18 -0500

    Adding a new (old) script to the repo for quick yt-dlp downloads with optimized presets

--- README.md
+++ README.md
@@ -3,6 +3,16 @@ Just a batch of scripts to make my life easier
 
   # fetch-discord.sh
  - An easy linux util for non-debian based installs to auto update discord when its out of date. I also added some support for the installers of BetterDiscord and Vencord for some personal taste!
+   ## Usecase
+   sh ./fetch-discord.sh 
    ## Features
     - -v flag for verbose installs to troubleshoot problems  
     - Betterdiscord and Vencord support for quick patching or installing if needed. 
+
+  # youtube-dl-preset.sh
+ - Small prompt script with a basic ytdlp preset to allow for highest quality audio and video with subtitile download support
+   ## Usecase
+   sh ./youtube-dl-preset.sh (video URL)
+   ## Features
+    - Highest quality audio and video downloading and merging
+    - Automatic english subtitle downloads
---
+++ youtube-dl-preset.sh
@@ -0,0 +1,17 @@
+# Make sure to change this location to the folder you want it to be moved too after
+# all the downloading is done. AKA i put it in the intake folder for manual review
+exitFolder='{enter exit folder here}'
+
+# get project name and move to the folder before starting the download
+echo "what is the name of the project?"
+read projName
+mkdir "$projName"
+cd "$projName"
+
+# Start downloading the link provided on execution
+yt-dlp.exe -vf "616+ba/620+ba/bv+ba/b" --write-subs --sub-lang en --write-auto-sub --convert-subs="srt" --cookies-from-browser firefox --no-check-certificate --add-metadata --convert-thumbnails jpg --ppa "ThumbnailsConvertor:-q:v 1" --embed-thumbnail -o "%(playlist_index)s - %(title)s.%(ext)s" -o "subtitle:%(title)s.%(ext)s" $1
+
+# Exit project dir and copy to the intake folder for manual review
+cd .. 
+# Not a good way to delete the file after the completion, will work on later
+mv -iuv "$projName" "$exitFolder"
